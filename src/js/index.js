96df0134921e3738011c9723c88f76412aa7dfda
Favour <ayofavour7@gmail.com>
2023-06-08 21:03:27 +0100

    feat: cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from 'gsap';
-// initSmoothScrolling();
-
-console.log("Hey! Let's build.");
 
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),

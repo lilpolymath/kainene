8b24bd6ceaa55aa0c6f931ff7dc7207de77f7ed3
Favour <ayofavour7@gmail.com>
2023-06-08 18:49:49 +0100

    feat[wip]: use case animation

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,10 @@ import initSmoothScrolling from './smooth-scoll';
 // initSmoothScrolling();
 
 console.log("Hey! Let's build.");
+
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+});

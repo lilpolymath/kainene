30570dfc8c500507b4ba4053ae0e3ce96102aef8
Favour <ayofavour7@gmail.com>
2023-06-08 20:28:22 +0100

    fix: mobile bug

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,8 @@ console.log("Hey! Let's build.");
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
+  smartphone: {
+    smooth: true,
+  },
+  smoothMobile: true,
 });

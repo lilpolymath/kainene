572cb30357f7c93f7a7c9499813be6bde39bea3c
Favour <ayofavour7@gmail.com>
2023-06-08 21:30:55 +0100

    fix: locomotive scroll for mobile

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,21 @@ import { gsap } from 'gsap';
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
+  // smartphone: {
+  //   smooth: true,
+  // },
+  // smoothMobile: true,
+  lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
+  multiplier: 1.4, // Effect Multiplier
+  reloadOnContextChange: true,
+  touchMultiplier: 2,
+  smoothMobile: 0,
   smartphone: {
-    smooth: true,
+    smooth: !0,
+    breakpoint: 767,
   },
-  smoothMobile: true,
+  tablet: {
+    smooth: !1,
+    breakpoint: 1024,
+  },
 });
